ee808c0fdc770f5c7ea3f387c996fdc1c2f084e6
Francesco Biedermann <francesco.biedermann@outlook.de>
2020-06-12 00:44:52 +0200

    done

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,30 @@
+class DosesController < ApplicationController
+
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(strong_params_dose)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail_id)
+  end
+
+  private
+
+  def strong_params_dose
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+end
