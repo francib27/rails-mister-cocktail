4de5de2e47e549deba8838a03889ea91b5caa6dc
Francesco Biedermann <francesco.biedermann@outlook.de>
2020-06-12 15:42:06 +0200

    test

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,29 +9,30 @@ Ingredient.create(name: "lemon")
 Ingredient.create(name: "ice")
 Ingredient.create(name: "mint leaves")
 
-# require "json"
-# require "open-uri"
+require "json"
+require "open-uri"
 
-# url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
 
-# Ingredient.destroy_all
-# json_string = open(url).read
+Dose.destroy_all
+Ingredient.destroy_all
+json_string = open(url).read
 
-# ruby_hash = JSON.parse(json_string)
-# ruby_hash["drinks"].map do |ingredient_baby_hash|
-#   Ingredient.create!(name: ingredient_baby_hash["strIngredient1"])
-# end
-
-puts "1.Cleaning database..."
-Cocktail.destroy_all
-
-puts "2.Creating cocktails..."
-sex_on_the_beach = { name: "Sex on the Beach" }
-moscow_mule =  { name: "Moscow Mule" }
-
-counter = 3
-[ sex_on_the_beach, moscow_mule].each do |attributes|
-  cocktail = Cocktail.create!(attributes)
-  puts "#{counter += 1} - Created #{cocktail.name}"
+ruby_hash = JSON.parse(json_string)
+ruby_hash["drinks"].each do |ingredient_baby_hash|
+  Ingredient.create!(name: ingredient_baby_hash["strIngredient1"])
 end
-puts "#{counter + 1}- Finished!"
+
+# puts "1.Cleaning database..."
+# Cocktail.destroy_all
+
+# puts "2.Creating cocktails..."
+# sex_on_the_beach = { name: "Sex on the Beach" }
+# moscow_mule =  { name: "Moscow Mule" }
+
+# counter = 3
+# [ sex_on_the_beach, moscow_mule].each do |attributes|
+#   cocktail = Cocktail.create!(attributes)
+#   puts "#{counter += 1} - Created #{cocktail.name}"
+# end
+# puts "#{counter + 1}- Finished!"
